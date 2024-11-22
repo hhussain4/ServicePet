ad4b399381ebd3861524ceee6fdad3d21e610980
Humayra Hussain <99496933+hhussain4@users.noreply.github.com>
2024-11-21 21:36:46 -0500

    Updated Login Style

--- pet_services.client/src/pages/dashboard.js
+++ pet_services.client/src/pages/dashboard.js
@@ -4,13 +4,18 @@ import './dashboard.css';
 
 const Dashboard = () => {
   return (
+    
+    
+    
+    
+    
     <div className="App">
       <div className="top-bar">
         <div className="logo">ServicePet</div>
         <div className="user-menu">
           <div className="user-icon">👤</div>
           <div className="dropdown">
-            <button className="dropbtn">☰</button>
+            <button className="dropbtn hover:bg-#0056b3">☰</button>
             <div className="dropdown-content">
               <Link to="/settings">User Settings</Link>
               <Link to="/logout">Sign Out</Link>
@@ -19,7 +24,6 @@ const Dashboard = () => {
         </div>
       </div>
       <div className="nav-bar">
-        <Link to="/dashboard" className="nav-link">Dashboard</Link>
         <Link to="/appointments" className="nav-link">Create Appointment</Link>
       </div>
       <div className="content">
--- pet_services.client/src/pages/login.js
+++ pet_services.client/src/pages/login.js
@@ -35,28 +35,28 @@ const Login = ({ onLogin }) => {
   };
 
   return (
-    <div className="login-container">
-      <div className="login-box">
-        <h1>Login</h1>
+    <div className="login-container flex justify-center items-center h-screen w-screen bg-[#F7ECE9]">
+      <div className="login-box bg-[#F1BABA] p-10 rounded-lg shadow-md w-[400px] max-w-full">
+        <h1 className= "mb-5 text-2xl text-center">Login</h1>
         {error && <p className="error">{error}</p>}
         <form onSubmit={handleSubmit}>
-          <label htmlFor="email">Email</label>
-          <input
+          <label htmlFor="email" className="block mb-1 font-bold text-left" >Email:</label>
+          <input className="w-full p-2 mb-5 border border-gray-300 rounded"
             type="email"
             id="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
           />
-          <label htmlFor="password">Password</label>
-          <input
+          <label htmlFor="password" className="block mb-1 font-bold text-left">Password:</label>
+          <input className="w-full p-2 mb-5 border border-gray-300 rounded"
             type="password"
             id="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
           />
-          <button type="submit">Login</button>
+          <button type="submit" className="w-full p-2 bg-[#AF4934] text-black border-none rounded cursor-pointer hover:bg-[#E4A79A] transition">Login</button>
         </form>
       </div>
     </div>
