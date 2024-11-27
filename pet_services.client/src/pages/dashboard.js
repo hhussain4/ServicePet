294db0698634fd1bcf0805d8c92e0fd535b5b84c
Kevin Morales <kevingustavo.kmf@gmail.com>
2024-11-27 17:58:48 -0500

    Fixed image

--- pet_services.client/src/pages/dashboard.js
+++ pet_services.client/src/pages/dashboard.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import NavBar from "./NavBar"
-
+import testImage from "../assets/test.jpeg";
 const Dashboard = () => {
   return (
     <div className="App">
       <NavBar />
       <div className="">
-        <img src="./assets/DashBanner.png"/>
+      <img src={testImage} alt="Test" />
       </div>
     </div>
   );
